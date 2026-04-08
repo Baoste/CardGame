4e0212e1f25f4ad8a7d26a1ca00b5fe6339f6558
Baoste <594096787@qq.com>
2026-04-08 17:33:10 +0800

    refactor: refact execute skill cards

--- Assets/Scripts/Net/NetEffectFunction.cs
+++ Assets/Scripts/Net/NetEffectFunction.cs
@@ -1,9 +1,94 @@
 using Game.Server;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Game.Domain
 {
     public static class NetEffectFunction
     {
+        public static void ExecuteEffectOp(int effectOpId, Card skillCard, MatchSession session, int playerId, int instanceId, ref CommandResult results)
+        {
+            while (effectOpId != -1)
+            {
+                EffectOp op = skillCard.effects[effectOpId];
+
+                // if the effect is judge, evaluate the judge result first, and then determine which node to go next based on the judge result
+                if (op.type == EffectType.Judge)
+                {
+                    List<int> judgePool = ParticipantsResolver.DetermineCandidates(op.source, session.gameState, session.ctx, out bool _);
+                    bool judgeResult = judgePool.Count > 0;
+                    if (judgeResult) effectOpId = op.trueNode;
+                    else effectOpId = op.falseNode;
+                    continue;
+                }
+
+                bool success = ParticipantsResolver.ReturnCandidates(
+                    op.source, op.target, session.gameState, session.ctx,
+                    out List<int> candidateSourceIds, out List<int> candidateTargetIds,
+                    out bool isSourceParticipantZone, out bool isTargetParticipantZone
+                );
+                if (!success)
+                {
+                    SendInvalidEvent(playerId, instanceId, results, InvalidActionType.InvalidTarget);
+                    if (effectOpId != 0)
+                        EndExecuteSkill(playerId, instanceId, session, results);
+                    return;
+                }
+
+                bool sourceNeedChoose = op.source.participantSelectionMode is SelectionModeChoose;
+                bool targetNeedChoose = op.target.participantSelectionMode is SelectionModeChoose;
+
+                // if need choose, send event to client to ask for participant selection,
+                // then wait for client's response to get selected participants and continue to execute the effect
+                if (sourceNeedChoose || targetNeedChoose)
+                {
+                    session.ctx.opStack.Push(new EffectOpExecutionContext
+                    {
+                        effectOp = op,
+                        candidateSourceIds = candidateSourceIds,
+                        candidateTargetIds = candidateTargetIds
+                    });
+                    results.events.Enqueue(CommandHandler.MakeEvent(
+                        "WaitForPlayer2Choose",
+                        new WaitForPlayer2ChooseEvent
+                        (
+                            playerId,
+                            success,
+                            instanceId,
+                            sourceNeedChoose,
+                            targetNeedChoose,
+                            isSourceParticipantZone,
+                            isTargetParticipantZone,
+                            candidateSourceIds,
+                            candidateTargetIds,
+                            op.source.maxSelectCount.Evaluate(session.gameState, session.ctx),
+                            op.target.maxSelectCount.Evaluate(session.gameState, session.ctx)
+                        ),
+                        playerId
+                    ));
+                    break;
+                }
+                // if not need choose, directly execute the effect
+                else
+                {
+                    List<int> selectedSourceIds, selectedTargetIds;
+                    int count;
+
+                    count = op.source.maxSelectCount.Evaluate(session.gameState, session.ctx);
+                    selectedSourceIds = op.source.participantSelectionMode.Execute(session.gameState, candidateSourceIds, count, new List<int>());
+                    count = op.target.maxSelectCount.Evaluate(session.gameState, session.ctx);
+                    selectedTargetIds = op.target.participantSelectionMode.Execute(session.gameState, candidateTargetIds, count, new List<int>());
+
+                    NetEffectExecutor.ExecuteOp(playerId, op, session, results, selectedSourceIds, selectedTargetIds);
+                }
+
+                if (op.trueNode == -1 && op.falseNode == -1) break;
+                else if (op.trueNode != -1 && op.falseNode == -1) effectOpId = op.trueNode;
+                else if (op.trueNode == -1 && op.falseNode != -1) effectOpId = op.falseNode;
+                else effectOpId = op.trueNode;
+            }
+        }
+
         public static void EndExecuteSkill(int playerId, int instanceId, MatchSession session, CommandResult results)
         {
             session.gameState.ClearResolve();
